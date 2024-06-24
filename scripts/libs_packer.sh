e705977b6fa2df16e4e177fc9644883cd16bf04a
ihexon <14349453+ihexon@users.noreply.github.com>
2024-06-24 11:21:19 +0000

    fix(libs_packer.sh): do not package anything if miss any libraries

--- scripts/libs_packer.sh
+++ scripts/libs_packer.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -e 
+set -e
 # libs_packer.sh
 # Analyze dependencies and package dependencies and target ELF files
 
@@ -19,8 +19,8 @@ set -e
 
 SHORTOPTS="lpvh"
 LONGOPTS="list,pack,version,help"
-LIST=true
-PACK=false
+LIST_FLAG=true
+PACK_FLAG=false
 
 PARSED=$(getopt --options=$SHORTOPTS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ $? -ne 0 ]]; then
@@ -53,11 +53,11 @@ while true; do
 		shift
 		;;
 	-l | --list)
-		LIST=true
+		LIST_FLAG=true
 		shift
 		;;
 	-p | --pack)
-		PACK=true
+		PACK_FLAG=true
 		shift
 		;;
 	--)
@@ -78,16 +78,41 @@ ELF_LIST=()
 ELF_LIST+="${*}"
 for elf in $ELF_LIST; do
 	echo "Analyze ${elf}..."
+
+	# Find out the libraries required in ${elf} installed on system
 	LIBS=()
-	LIBS+=$(ldd ${elf} | grep -v linux-vdso | cut -d '>' -f2 | cut -d '(' -f1)
+	LIBS+=$(ldd ${elf} | grep -v linux-vdso | grep -v 'not found' | cut -d '>' -f2 | cut -d '(' -f1)
 	LIBS+=${elf}
-	
-	LIST=$(echo "${LIBS[@]}" | xargs)
-	for wb in $LIST; do
-		test ${PACK} = "true" && {
-			OUTPUT_DIR=${OUTPUT_DIR:="/tmp/"}
-			echo tar --dereference -rvf "${OUTPUT_DIR}/deps.tar" "${wb}"
-			tar --dereference -rvf "${OUTPUT_DIR}/deps.tar" "${wb}" > /dev/null 2>&1 
-		} || echo "  ${elf} require: ${wb}"
+
+	# Find out the libraries required in ${elf} but not installed on system
+	LIBS_NOT_INSTALLED=()
+	LIBS_NOT_INSTALLED=$(ldd ${elf} | grep -v linux-vdso | grep 'not found' | cut -d '=' -f1 | xargs)
+
+	for wb in ${LIBS_NOT_INSTALLED[*]}; do
+		test ${LIST_FLAG} = "true" && {
+			echo "  ${elf} require but NOT IN SYSTEM: ${wb}"
+		}
+	done
+
+	for wb in ${LIBS[*]}; do
+		test ${LIST_FLAG} = "true" && {
+			echo "  ${elf} require: ${wb}"
+		}
+	done
+
+	for wb in ${LIBS[*]}; do
+		# If any of the dependencies are not installed on the system, we shouldn't package anything at all.
+		test ${PACK_FLAG} = "true" && {
+			test -z "${LIBS_NOT_INSTALLED[*]}" && {
+				OUTPUT_DIR=${OUTPUT_DIR:="/tmp/"}
+				echo "  tar --dereference -rvf ${OUTPUT_DIR}/deps.tar ${wb}"
+				tar --dereference -rvf "${OUTPUT_DIR}/deps.tar" "${wb}" >/dev/null 2>&1
+
+			} || {
+				echo "  Error:"
+				echo "    ${LIBS_NOT_INSTALLED[*]} not installed on system, can not do pack"
+				exit 100
+			}
+		}
 	done
 done
