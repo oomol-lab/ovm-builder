5346786643e2366c58232e5dc878ad316377aaf8
ihexon <14349453+ihexon@users.noreply.github.com>
2024-07-11 06:46:43 +0000

    chore(script): deprecation of tee-ovm

--- scripts/logger_wrapper.sh
+++ scripts/logger_wrapper.sh
@@ -1,35 +1,35 @@
 #! /bin/sh
 
-# Wraps awk to redirect error and stdout to `tee-ovm`
+# Wraps awk to redirect error and stdout to `ovmd`
 # This script works with openrc's supervise-daemon as a logger
-# If tee-ovm running...
-# - stdout to `/proc/${TEE_OVM_PID}/fd/1`
-# - stderr to `/proc/${TEE_OVM_PID}/fd/2`
-# If tee-ovm not running...
+# If ovmd running...
+# - stdout to `/proc/${OVMD_PID}/fd/1`
+# - stderr to `/proc/${OVMD_PID}/fd/2`
+# If ovmd not running...
 # - stdout to `/tmp/ovm_log_stdout.log`
 # - stderr to `/tmp/ovm_log_stderr.log`
 
-TEE_OVM_PID=$(busybox ps aux | grep -v grep | grep tee-ovm | xargs | busybox cut -d ' ' -f1)
+OVMD_PID=$(busybox ps aux | grep -v grep | grep ovmd | xargs | busybox cut -d ' ' -f1)
 
-test -z ${TEE_OVM_PID} && {
-	echo "ovm-tee not running, all message log to /tmp/ovm_log_{stderr,stdout}.log"
-	LOG_TYPE="filebased" 
+test -z ${OVMD_PID} && {
+	echo "ovmd not running, all message log to /tmp/ovm_log_{stderr,stdout}.log"
+	LOG_TYPE="file" 
 } || {
-	echo "ovm-tee running, all mesage redirect to tee-ovm"
-	LOG_TYPE="teebased"
+	echo "ovmd running, all mesage redirect to ovmd"
+	LOG_TYPE="process"
 }
 
 logger_stdout() {
-	test ${LOG_TYPE} = "teebased" && {
-		busybox awk '{ print ENVIRON["RC_SVCNAME"] " " $0 }' >/proc/${TEE_OVM_PID}/fd/1
+	test ${LOG_TYPE} = "process" && {
+		busybox awk '{ print ENVIRON["RC_SVCNAME"] " " $0 }' >/proc/${OVMD_PID}/fd/1
 	} || {
 		busybox awk '{ print ENVIRON["RC_SVCNAME"] " " $0 }' >>/tmp/ovm_log_stdout.log
 	}
 }
 
 logger_stderr() {
-	test ${LOG_TYPE} = "teebased" && {
-		busybox awk '{ print ENVIRON["RC_SVCNAME"] " " $0 }' >/proc/${TEE_OVM_PID}/fd/2
+	test ${LOG_TYPE} = "process" && {
+		busybox awk '{ print ENVIRON["RC_SVCNAME"] " " $0 }' >/proc/${OVMD_PID}/fd/2
 	} || {
 		busybox awk '{ print ENVIRON["RC_SVCNAME"] " " $0 }' >>/tmp/ovm_log_stderr.log
 	}
