bb187075f8c04595fc2dd541a9bab58bd0b38e2a
CAT <14349453+ihexon@users.noreply.github.com>
2025-02-13 23:53:57 +0800

    refactor(builder): simplify builder code logic (#52)
    This PR reduces the complexity of the builder (with 364 additions and
    811 deletions)

--- subfunc/install_package.sh
+++
@@ -1,8 +0,0 @@
-if [[ $SKIP_APT_GET_INSTALL == "true" ]]; then
-	echo "Warring: Skip apt install package required"
-else
-	set -ex
-	sudo -E apt update
-	sudo -E apt install -y libarchive-dev libtalloc-dev uthash-dev build-essential git
-	set -xe
-fi
--- subfunc/install_qemu.sh
+++ subfunc/install_qemu.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
+set -e
+# keep this script test able means this script do not require any envs
+# from caller
 x86_64_loader_ld="ld-linux-x86-64.so.2"
 arm64_loader_ld="ld-linux-aarch64.so.1"
 
-sudo -E apt install wget
-
-host_arch=$(uname -m)
+host_arch="$(uname -m)"
 if [[ $host_arch == aarch64 ]] || [[ $host_arch == arm64 ]]; then
 	host_arch=arm64
 	ld_loader="$arm64_loader_ld"
@@ -16,47 +17,51 @@ if [[ $host_arch == amd64 ]] || [[ $host_arch == x86_64 ]]; then
 fi
 
 check_envs() {
-	test -n $output || {
-		echo 'Env: output failed'
+	test -n "${output:?}" || {
+		echo 'env: output failed'
 		exit 100
 	}
 
-	test -n $workspace || {
-		echo 'Env: workspace failed'
+	test -n "${workspace:?}" || {
+		echo 'env: workspace failed'
 		exit 100
 	}
 
-	test -n $host_arch || {
-		echo 'Env: host_arch failed'
+	test -n "${host_arch:?}" || {
+		echo 'env: host_arch failed'
+		exit 100
+	}
+	test -n "${ld_loader:?}" || {
+		echo 'env: ld_loader failed'
 		exit 100
 	}
 }
 
 download_qemu() {
-	if [[ "$(uname -s)" == "Linux" ]]; then
-		echo "INFO: Download qemu_bin-linux-$host_arch.tar.xz"
-		set -xe
-		sudo -E apt -y install xz-utils tar binutils zstd
-		mkdir -p "$output" && cd "$output"
-		wget "https://github.com/oomol/builded/releases/download/v1.6/qemu_bin-linux-$host_arch.tar.xz" \
-			--output-document="$output/qemu_bin-linux-$host_arch.tar.xz" \
-			--output-file=/tmp/wget_qemu.log
-		tar -xvf "qemu_bin-linux-$host_arch.tar.xz" -C ./ >/dev/null
-		cd "$workspace"
-		set +xe
-		echo "INFO: Download qemu_bin-linux-$host_arch.tar.xz done"
-	fi
+
+	echo "download qemu_bin-linux-$host_arch.tar.xz"
+	sudo -E apt update
+	sudo -E apt -y install xz-utils tar binutils zstd wget
+
+	qemu_tar="$output/qemu_bin-linux-$host_arch.tar.xz"
+
+	mkdir -p "$output"
+	wget -c "https://github.com/oomol/builded/releases/download/v1.6/qemu_bin-linux-$host_arch.tar.xz" \
+		--output-document="$qemu_tar" \
+		--output-file="$(dirname "$qemu_tar")/qemu_wget.log"
+	echo "download $qemu_tar finished"
+
+	tar -xvf "$qemu_tar" -C "$output" >"$output/qemu_tar.log"
+	echo "extract $qemu_tar finished"
 }
 
 test_qemu_bin() {
-	set -xe
-	cd "$output"
-	./qemu_bins/lib/$ld_loader --library-path ./qemu_bins/lib ./qemu_bins/bin/qemu-system-x86_64 --version >/dev/null
-	./qemu_bins/lib/$ld_loader --library-path ./qemu_bins/lib ./qemu_bins/bin/qemu-system-aarch64 --version >/dev/null
-	./qemu_bins/static_qemu/bin/qemu-x86_64 --version >/dev/null
-	./qemu_bins/static_qemu/bin/qemu-aarch64 --version >/dev/null
-	set -xe
-	cd "$workspace"
+	ld_loader="$output/qemu_bins/lib/$ld_loader"
+	libs_dir="$(dirname "$ld_loader")"
+	"$ld_loader" --library-path "$libs_dir" "$output/qemu_bins/bin/qemu-system-x86_64" --version
+	"$ld_loader" --library-path "$libs_dir" "$output/qemu_bins/bin/qemu-system-aarch64" --version
+	"$output/qemu_bins/static_qemu/bin/qemu-x86_64" --version
+	"$output/qemu_bins/static_qemu/bin/qemu-aarch64" --version
 }
 
 check_envs
